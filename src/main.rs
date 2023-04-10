2a600fa1b1aa8120ce3dae3ca01e0926551883eb
Jarl André Hübenthal <jarlah@protonmail.com>
2023-04-10 05:08:46 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -10,50 +10,59 @@ use std::fs;
 #[grammar = "cv.pest"]
 pub struct CVParser;
 
-fn get_value(pair: Pair<Rule>) -> Result<&str, String> {
+fn get_value(pair: Pair<Rule>) -> Result<&str, ()> {
     let mut inner_rules = pair.into_inner();
-    let val = inner_rules.next().ok_or("no value")?.as_str();
+    let val = inner_rules
+        .next()
+        .ok_or("no value")
+        .map_err(|err| eprintln!("ERROR: {err}"))?
+        .as_str();
     Ok(val)
 }
 
-fn main() -> Result<(), ()> {
-    let unparsed_file = fs::read_to_string("cv.ini")
-        .map_err(|err| eprintln!("ERROR: Failed to read file: {err}"))?;
+fn read_file(file: &str) -> Result<String, ()> {
+    fs::read_to_string(file).map_err(|err| eprintln!("ERROR: Failed to read file: {err}"))
+}
 
-    let file = CVParser::parse(Rule::cv, &unparsed_file)
+fn parse_file(file: &str) -> Result<Pair<Rule>, ()> {
+    CVParser::parse(Rule::cv, file)
         .map_err(|err| eprintln!("ERROR: Failed to parse file: {err}"))?
         .next()
-        .ok_or_else(|| eprintln!("ERROR: File is empty"))?;
+        .ok_or_else(|| eprintln!("ERROR: File is empty"))
+}
 
-    let mut properties: HashMap<&str, HashMap<&str, &str>> = HashMap::new();
+fn parse_about(rule: Pair<Rule>) -> Result<HashMap<&str, String>, ()> {
+    let mut about_section: HashMap<&str, String> = HashMap::new();
+    for inner_rule in rule.into_inner() {
+        match inner_rule.as_rule() {
+            Rule::email => {
+                let value = get_value(inner_rule)?.to_owned();
+                about_section.insert("email", value);
+            }
+            Rule::phone => {
+                let value = get_value(inner_rule)?.to_owned();
+                about_section.insert("phone", value);
+            }
+            Rule::EOI => todo!(),
+            _ => unreachable!(),
+        }
+    }
+    Ok(about_section)
+}
 
+fn main() -> Result<(), ()> {
+    let unparsed_file = read_file("cv.ini")?;
+    let file = parse_file(&unparsed_file)?;
+    let mut properties: HashMap<&str, HashMap<&str, String>> = HashMap::new();
     for line in file.into_inner() {
         match line.as_rule() {
             Rule::about => {
-                let section = properties.entry("about").or_default();
-                for inner_rule in line.into_inner() {
-                    match inner_rule.as_rule() {
-                        Rule::email => {
-                            let value =
-                                get_value(inner_rule).map_err(|err| eprintln!("ERROR: {err}"))?;
-                            section.insert("email", value);
-                        }
-                        Rule::phone => {
-                            let value =
-                                get_value(inner_rule).map_err(|err| eprintln!("ERROR: {err}"))?;
-                            section.insert("phone", value);
-                        }
-                        Rule::EOI => todo!(),
-                        _ => unreachable!(),
-                    }
-                }
+                properties.insert("about", parse_about(line)?);
             }
             Rule::EOI => (),
             _ => unreachable!(),
         }
     }
-
     println!("{:#?}", properties);
-
     Ok(())
 }
