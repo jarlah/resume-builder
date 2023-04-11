3999ab58857d928d9da5367a3368f31e37e53588
Jarl André Hübenthal <jarlah@protonmail.com>
2023-04-11 09:15:26 +0200

    derive default

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,15 @@ use std::fs;
 #[grammar = "cv.pest"]
 pub struct CVParser;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct About {
     pub phone: String,
     pub email: String
 }
 
-impl About {
-    fn new() -> About {
-        About { phone: "".to_owned(), email: "".to_owned() }
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct CVData {
     pub about: About,
-}
-
-impl CVData {
-    fn new() -> CVData {
-        CVData { about: About::new() }
-    }
 }
 
 fn get_value(pair: Pair<Rule>) -> Result<&str, ()> {
@@ -71,7 +59,7 @@ fn parse_about(rule: Pair<Rule>, about: &mut About) -> Result<(), ()> {
 fn main() -> Result<(), ()> {
     let unparsed_file = read_file("cv.ini")?;
     let file = parse_file(&unparsed_file)?;
-    let mut cv_data: CVData = CVData::new();
+    let mut cv_data: CVData = Default::default();
     for line in file.into_inner() {
         match line.as_rule() {
             Rule::about => parse_about(line, &mut cv_data.about)?,
